e325d5b824f4b688723925ffbb855e98056097a5
ur64n <jakub.kaczmarczyk443@gmail.com>
2025-12-13 17:58:51 +0000

    feat(etl): add csv to delta transformation and UC write
    - Added new module "transformation" in class.
    - Added CSV → Delta transformation method in new module
    - Persisted data as Delta table in Unity Catalog

--- src/config/config.py
+++ src/config/config.py
@@ -1,3 +1,5 @@
 # paths
 raw_csv_files_path = "/Volumes/workspace/bronze/raw/"
 settings_path = "/Workspace/Users/jakub.kaczmarczyk443@gmail.com/dbx-invest-analytics/src/config/settings.yaml"
+raw_delta_files_path = "workspace.bronze.qqq_etf_constituents"
+qqq_entities_filename = "qqq-etf-constituents.csv"
