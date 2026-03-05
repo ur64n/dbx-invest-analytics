ee68aa0ee894d446a6f85e7d7e58ab7a5d158159
ur64n <jakub.kaczmarczyk443@gmail.com>
2026-03-05 10:41:18 +0000

    feat: add bronze table read step to categories pipeline

--- src/pipelines/run_qqq_categories_bronze_to_silver.py
+++ src/pipelines/run_qqq_categories_bronze_to_silver.py
@@ -2,6 +2,8 @@ from pyspark.sql import SparkSession
 from src.config.config_loader import load_config
 from src.config.logger import get_logger
 
+from src.etl.extraction.delta_table_extractor import DeltaTableExtractor
+
 logger = get_logger("qqq_categories_bronze_to_silver")
 
 def main():
@@ -10,7 +12,14 @@ def main():
     # ---------- setup ----------
     spark = SparkSession.builder.getOrCreate()
     config = load_config()
-    
+
+    # ---------- read data ----------
+    table_name = config["tables"]["bronze_qqq"]
+
+    df = DeltaTableExtractor(
+        spark=spark,
+        table_name=table_name
+    ).read()
 
 if __name__ == "__main__":
     main()
