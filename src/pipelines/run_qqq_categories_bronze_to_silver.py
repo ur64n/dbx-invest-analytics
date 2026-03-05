a570556d7f68dd676300995ffde8ac24dbf5d97a
ur64n <jakub.kaczmarczyk443@gmail.com>
2026-03-05 11:16:47 +0000

    fix: mistake with pipeline names

--- src/pipelines/run_qqq_categories_bronze_to_silver.py
+++ src/pipelines/run_qqq_categories_bronze_to_silver.py
@@ -1,25 +1,15 @@
 from pyspark.sql import SparkSession
-from src.config.config_loader import load_config
 from src.config.logger import get_logger
-
-from src.etl.extraction.delta_table_extractor import DeltaTableExtractor
+from src.config.config_loader import load_config
 
 logger = get_logger("qqq_categories_bronze_to_silver")
 
-def main():
-    logger.info("Starting qqq_categories_bronze_to_silver")
+def run():
+    logger.info("Starting qqq_categories_bronze_to_silver pipeline")
 
     # ---------- setup ----------
     spark = SparkSession.builder.getOrCreate()
     config = load_config()
 
-    # ---------- read data ----------
-    table_name = config["tables"]["bronze_qqq"]
-
-    df = DeltaTableExtractor(
-        spark=spark,
-        table_name=table_name
-    ).read()
-
 if __name__ == "__main__":
-    main()
+    run()
--- src/pipelines/run_qqq_entities_bronze_to_silver.py
+++ src/pipelines/run_qqq_entities_bronze_to_silver.py
@@ -1,27 +1,28 @@
 from pyspark.sql import SparkSession
-from src.config.logger import get_logger
 from src.config.config_loader import load_config
+from src.config.logger import get_logger
 
 from src.etl.extraction.delta_table_extractor import DeltaTableExtractor
 
 logger = get_logger("qqq_entities_bronze_to_silver")
 
 def run():
-    logger.info("Starting qqq_entities_bronze_to_silver pipeline")
+    logger.info("Starting qqq_entities_bronze_to_silver")
 
     # ---------- setup ----------
     spark = SparkSession.builder.getOrCreate()
     config = load_config()
-    
-    # ---------- extraction ----------
-    extractor = DeltaTableExtractor(
-        spark,
-        table_name=config["tables"]["bronze_qqq"]
-    )
 
-    df = extractor.read()
+    # ---------- read data ----------
+    table_name = config["tables"]["bronze_qqq"]
 
-    df.limit(10).display()
+    df = DeltaTableExtractor(
+        spark=spark,
+        table_name=table_name
+    ).read()
+
+    # ---------- cleaning ----------
+    df = 
 
 if __name__ == "__main__":
     run()
