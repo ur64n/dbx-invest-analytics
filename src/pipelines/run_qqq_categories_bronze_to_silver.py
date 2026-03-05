2d868db61c27a42bfeeb189bda923200da7f7212
ur64n <jakub.kaczmarczyk443@gmail.com>
2026-03-05 13:59:42 +0000

    feat: add bronze delta table read

--- src/pipelines/run_qqq_categories_bronze_to_silver.py
+++ src/pipelines/run_qqq_categories_bronze_to_silver.py
@@ -2,6 +2,8 @@ from pyspark.sql import SparkSession
 from src.config.logger import get_logger
 from src.config.config_loader import load_config
 
+from src.etl.extraction.delta_table_extractor import DeltaTableExtractor
+
 logger = get_logger("qqq_categories_bronze_to_silver")
 
 def run():
@@ -10,6 +12,16 @@ def run():
     # ---------- setup ----------
     spark = SparkSession.builder.getOrCreate()
     config = load_config()
+    
+    # ---------- read data ----------
+    df = DeltaTableExtractor(
+        spark=spark,
+        table_name=config["tables"]["bronze_qqq_categories"]
+    ).read()
+
+    # ---------- cleaning ----------
+    
+
 
 if __name__ == "__main__":
     run()
