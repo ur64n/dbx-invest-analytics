a3a22251ceacd1d07a98e1cc37a29f68a00157c6
ur64n <jakub.kaczmarczyk443@gmail.com>
2026-03-09 19:37:28 +0000

    feat: Add write step in gold_fred_with_dimension pipeline

--- src/pipelines/run_gold_fred_with_dimension.py
+++ src/pipelines/run_gold_fred_with_dimension.py
@@ -4,6 +4,7 @@ from src.config.logger import get_logger
 
 from src.etl.extraction.delta_table_extractor import DeltaTableExtractor
 from src.etl.enrichment.fred_dimension_enrichment import FredDimensionEnricher
+from src.etl.write.delta_table_writer import DeltaTableWriter
 
 logger = get_logger("gold_fred_with_dimension pipeline")
 
@@ -30,9 +31,15 @@ def run():
     logger.info("Enrichment completed successfully")
 
     # ---------- validation ----------
-    
+    assert df.count() == fact_df.count(), "Row count mismatch after join"
 
+    # ---------- write ----------
+    DeltaTableWriter(
+        table_name=config["tables"]["gold_fred_with_dimension"],
+        spark=spark
+    ).overwrite(df)
     
+    logger.info("gold_fred_with_dimension pipeline completed successfully")
 
 if __name__ == "__main__":
     run()
