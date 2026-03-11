b2aab3bf3e35cb17956ffb234e4aba5593f3dc54
ur64n <jakub.kaczmarczyk443@gmail.com>
2026-03-11 09:00:20 +0000

    feat: Add extraction step in gdelt pipeline

--- src/pipelines/run_gdelt_news_api_to_bronze.py
+++ src/pipelines/run_gdelt_news_api_to_bronze.py
@@ -2,6 +2,8 @@ from pyspark.sql import SparkSession
 from src.config.config_loader import load_config
 from src.config.logger import get_logger
 
+from src.etl.extraction.gdelt.gdelt_client import GdeltClient
+
 logger = get_logger("gdelt_new_api_to_bronze")
 
 def run():
@@ -12,7 +14,12 @@ def run():
     config = load_config()
 
     # ---------- extractor ----------
+    for kw in config["gdelt"]["keywords"]:
     
+    gdelt_data = GdeltClient(
+        config=config,
+        spark=spark
+    ).fetch_articles(kw)
 
 if __name__ == "__main__":
     run()
