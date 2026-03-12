a273bcdbfbbfbfc260c51fc23d595d6d10423880
ur64n <jakub.kaczmarczyk443@gmail.com>
2026-03-12 13:20:17 +0000

    Delete gdelt pipeline and other features and add new feat with upsert logic in DeltTableWriter

--- src/etl/write/delta_table_writer.py
+++ src/etl/write/delta_table_writer.py
@@ -1,5 +1,6 @@
 from src.config.logger import get_logger
 from pyspark.sql import DataFrame
+from delta.tables import DeltaTable
 
 logger = get_logger("delta_table_writer")
 
@@ -12,3 +13,28 @@ class DeltaTableWriter:
         logger.info(f"Writing {df.count()} rows to {self.table_name}")
 
         df.write.format("delta").mode("overwrite").saveAsTable(self.table_name)
+
+    def upsert(self, df: DataFrame, merge_keys: list[str]) -> None:
+        logger.info(f"Upserting {df.count()} rows into {self.table_name}")
+
+        if not self.spark.catalog.tableExists(self.table_name):
+            df.write.format("delta").mode("overwrite").saveAsTable(self.table_name)
+            
+            return
+
+        delta_table = DeltaTable.forName(self.spark, self.table_name)
+
+        merge_condition = " AND ".join(
+            [f"target.{key} = source.{key}" for key in merge_keys]
+        )
+
+        delta_table.alias("target").merge(
+            df.alias("source"),
+            merge_condition
+        ) \
+        .whenMatchedUpdateAll() \
+        .whenNotMatchedInsertAll() \
+        .execute()
+
+
+
