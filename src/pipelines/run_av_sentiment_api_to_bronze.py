52dd1a69596a6420c7c8346ecb59efde524028b1
ur64n <jakub.kaczmarczyk443@gmail.com>
2026-03-13 11:31:56 +0000

    feat: Add parameters and prepared symbol list for extraction

--- src/pipelines/run_av_sentiment_api_to_bronze.py
+++ src/pipelines/run_av_sentiment_api_to_bronze.py
@@ -8,6 +8,8 @@ from pyspark.dbutils import DButils
 from src.config.logger import get_logger
 from src.config.config_loader import load_config
 
+from src.etl.extraction.delta_table_extractor import DeltaTableExtractor
+
 logger = get_logger("av_sentiment_api_to_bronze")
 
 def run():
@@ -22,7 +24,25 @@ def run():
     api_key = dbutils.secrets.get("my-scope","API_KEY_AV")
 
     # ---------- parameters ----------
-    
+    rate_limit = config["alpha_vantage"]["rate_limit_per_min"]
+    limit_per_request = config["alpha_vantage"]["articles_per_request"]
+
+    run_ts = datetime.now(ZoneInfo("Europe/Warsaw"))
+
+    # ---------- load symbols from silver_qqq ----------
+    entities_df = DeltaTableExtractor(
+        spark=spark,
+        table_name=config["tables"]["silver_qqq"]
+    ).read()
+
+    symbols = [
+        row["symbol"].upper()
+        for row in entities_df.select("symbol").distinct().collect()
+    ]
+
+    logger.info(f"Total symbols to fetch sentiment for {len(symbols)}")
+
+    # ---------- extraction + parsing ----------
 
 if __name__ == "__main__":
     run()
