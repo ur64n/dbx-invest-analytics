0db9ac93306fefdf421321cdf5ef2d50fafb44ea
ur64n <jakub.kaczmarczyk443@gmail.com>
2026-03-18 16:17:54 +0000

    add gold sentiment validation: uniqueness, score ranges, non-negative counts

--- src/etl/validation/gold_sentiment_validation.py
+++ src/etl/validation/gold_sentiment_validation.py
@@ -1,10 +1,52 @@
 from pyspark.sql import DataFrame
 from src.config.logger import get_logger
 
+from pyspark.sql.functions import col
+
 logger = get_logger("gold_sentiment_validation")
 
 class GoldAVSentimentValidator:
 
     @staticmethod
-    def validate_emptiness(df: DataFrame) -> None:
-        return None
+    def validate_uniqueness(df: DataFrame) -> None:
+        logger.info("Starting uniqueness validation on (symbol+date) columns")
+        
+        total = df.count()
+        unique_rows = df.select("symbol","date").distinct().count()
+
+        if total != unique_rows:
+            raise ValueError("Dataset contains duplicated values")
+
+        logger.info("Complete, all rows are unique")
+
+    @staticmethod
+    def validate_domain_rules(df: DataFrame) -> None:
+
+        logger.info("Validating all symbols contain articles data")
+
+        if df.filter(col("article_count") <= 0).limit(1).count() > 0:
+            raise ValueError("Some symbols have no articles sentiment data")
+
+        logger.info("Validating avg_sentiment_score correct value range")
+
+        if df.filter((col("avg_sentiment_score") > 1) | (col("avg_sentiment_score") < -1)).head(1):
+            raise ValueError("Values avg_sentiment_score is out of range")
+
+        logger.info("Validating avg_relevance_score correct value range")
+
+        if df.filter((col("avg_relevance_score") > 1) | (col("avg_relevance_score") < 0)).head(1):
+            raise ValueError("Values avg_relevance_score is out of range")
+
+        logger.info("Validating non negative values in count columns")
+
+        if df.filter(
+            (col("bullish_count") < 0) |
+            (col("somewhat_bullish_count") < 0) |
+            (col("neutral_count") < 0) |
+            (col("somewhat_bearish_count") < 0) |
+            (col("bearish_count") < 0)
+        ).limit(1).count() > 0:
+            raise ValueError("Some of count columns contain negative value")
+
+        
+
--- src/pipelines/run_gold_sentiment_daily_agg.py
+++ src/pipelines/run_gold_sentiment_daily_agg.py
@@ -7,7 +7,7 @@ from src.etl.schema.av_schema import GOLD_REQUIRED_COLUMNS
 from src.etl.extraction.delta_table_extractor import DeltaTableExtractor
 from src.etl.transformations.av_sentiment_aggregation import AVSentimentAggregator
 from src.etl.utils.validation_helper import ValidationHelper
-from src.etl.validation.gold_sentiment_validation import GoldAVVSentimentalidator
+from src.etl.validation.gold_sentiment_validation import GoldAVSentimentalidator
 
 logger = get_logger("gold_sentiment_daily_agg")
 
@@ -30,8 +30,11 @@ def run():
     # ---------- validation ----------
     ValidationHelper.validate_not_empty(df, context="")
     ValidationHelper.validate_schema(df, GOLD_REQUIRED_COLUMNS, context="gold sentiment aggregated df")
-
-
+    GoldAVSentimentalidator.validate_uniqueness(df)
+    GoldAVSentimentalidator.validate_domain_rules(df)
+    
+    # ---------- write ----------
+    
 
 if __name__ == "__main__":
     run()
