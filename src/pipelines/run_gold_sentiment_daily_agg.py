554c877b03d2d708a99b0b45c441335c18cd5128
ur64n <jakub.kaczmarczyk443@gmail.com>
2026-03-18 10:35:04 +0000

    Feat: Add skeleton for gold av sentiment daily aggregation pipeline

---
+++ src/pipelines/run_gold_sentiment_daily_agg.py
@@ -0,0 +1,21 @@
+from pyspark.sql import SparkSession
+from src.config.config_loader import load_config
+from src.config.logger import get_logger
+
+from src.etl.extraction.delta_table_extractor import DeltaTableExtractor
+
+logger = get_logger("gold_sentiment_daily_agg")
+
+def run():
+    logger.info("Starting gold sentiment daily aggregation pipeline")
+
+    # ---------- setup ----------
+    spark = SparkSession.builder.getOrCreate()
+    config = load_config()
+
+    # ---------- read data ----------
+    
+
+
+if __name__ == "__main__":
+    run()
