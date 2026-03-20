0da4f60a55d46c1608e60cad7f50e7e45723c4f5
ur64n <jakub.kaczmarczyk443@gmail.com>
2026-03-20 10:28:35 +0000

    feat: Add window function

--- src/etl/transformations/ohlcv_return_calculation.py
+++ src/etl/transformations/ohlcv_return_calculation.py
@@ -1,5 +1,6 @@
 from pyspark.sql import DataFrame
-from pyspark.sql.functions import x
+from pyspark.sql.functions import lag, col
+from pyspark.sql.window import Window
 from src.config.logger import get_logger
 
 logger = get_logger("ohlcv_return_calculation")
@@ -8,5 +9,8 @@ class OHLCVCalculator:
 
     def xyz(df: DataFrame) -> DataFrame:
         logger.info("Starting...")
+
+        window = Window.partitionBy("symbol").orderBy("date")
+        df = df.withColumn("prev_close", lag("close", 1).over(window))
 
         
